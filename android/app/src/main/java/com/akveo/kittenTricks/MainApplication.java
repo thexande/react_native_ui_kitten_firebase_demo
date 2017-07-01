c574a4eadc6b26f3e79e62ddd3aa161bfb8c3b1d
Alexander Murphy <amurphy345@gmail.com>
2017-07-01 01:17:58 -0600

    integrating firebase into android

--- android/app/src/main/java/com/akveo/kittenTricks/MainApplication.java
+++ android/app/src/main/java/com/akveo/kittenTricks/MainApplication.java
@@ -1,10 +1,25 @@
 package com.akveo.kittenTricks;
 
+
+// Required package
+import io.invertase.firebase.RNFirebasePackage; // <-- Add this line
+// Optional packages - add as appropriate
+import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
+import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
+import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
+import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Messaging
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
+
+
 import android.app.Application;
 
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactApplication;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
+// import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +42,20 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new GoogleAnalyticsBridgePackage(),
+          
+          new RNFirebasePackage(),  // <-- Add this line
+          // Add these packages as appropriate
+          new RNFirebaseAdMobPackage(),
+          new RNFirebaseAnalyticsPackage(),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseRemoteConfigPackage(),
+          new RNFirebaseCrashPackage(),
+          new RNFirebaseDatabasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebasePerformancePackage(),
+          new RNFirebaseStoragePackage(),
+          
+          // new GoogleAnalyticsBridgePackage(),
           new SvgPackage(),
           new LinearGradientPackage()
 
